aee64bdd1bd7ff3a38e1a2ac6d086ad4e56b8961
xBeke <ambrus.beke@gmail.com>
2020-10-20 07:21:23 +0200

    Add box class

---
+++ src/Box.java
@@ -0,0 +1,13 @@
+public class Box {
+    public int height;
+    public int width;
+    public int area;
+    public int id;
+
+    public Box(int h, int w, int i) {
+        height = h;
+        width = w;
+        area = h*w;
+        id = i;
+    }
+}
