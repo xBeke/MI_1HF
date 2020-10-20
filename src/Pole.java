5d4dfde3ca1801ba78dfc957e93837b29620adde
xBeke <ambrus.beke@gmail.com>
2020-10-20 07:47:01 +0200

    Add pole and repo classes

---
+++ src/Pole.java
@@ -0,0 +1,10 @@
+
+public class Pole {
+    public int x;
+    public int y;
+
+    public Pole(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/Repository.java
@@ -0,0 +1,31 @@
+import com.sun.jdi.ArrayReference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Repository {
+    public int height;
+    public int width;
+    public ArrayList<Pole> poles = new ArrayList<Pole>();
+    public ArrayList<Box> boxInputBuffer = new ArrayList<Box>();
+    public int [][] boxes;
+    public int numberOfBoxes;
+    public int numberOfPoles;
+
+    public Repository(int height, int width, int[][] boxes) {
+        this.height = height;
+        this.width = width;
+        this.boxes = new int[height][width];
+        Arrays.fill(this.boxes, 0);
+        this.numberOfBoxes = 0;
+        this.numberOfPoles = 0;
+    }
+
+    public void addBox(int height, int width, int id){
+        boxInputBuffer.add(new Box(height, width, id));
+    }
+
+    public void addPole(int x, int y){
+        poles.add(new Pole(x ,y));
+    }
+}
